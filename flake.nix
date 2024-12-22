a35b793e86ae4a3e16cc75ff6ef256d769438a38
yeeha.zerog <yeeha.zerog@gmail.com>
2024-12-22 14:15:41 +0100

    broken branch

--- flake.nix
+++ flake.nix
@@ -27,10 +27,10 @@
                 (old: {
                   nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ map (a: final.${a}) systems;
                 }))
-              {
+              #{
                 # https://github.com/nix-community/poetry2nix/blob/master/docs/edgecases.md#modulenotfounderror-no-module-named-packagename
                 # package = [ "setuptools" ];
-              }
+              #}
           );
         };
         pkgs = import nixpkgs {
